e64e04aac1f2209cd287890a9ebb439463006640
shu yu Hsu <shuyuhsu@shus-MacBook-Air.local>
2022-10-17 19:22:22 +0800

    clean code

--- linebot0817/write_in_database.py
+++ linebot0817/write_in_database.py
@@ -7,54 +7,13 @@ from botocore.exceptions import ClientError
 import datetime
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
-def button(event):
-    if isinstance(event, MessageEvent):
-        if event.message.text == "hi":
-          
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-            event.reply_token,
-            TemplateSendMessage(
-    alt_text='ButtonsTemplate',
-    template=ButtonsTemplate(
-                            title='Menu',
-                            text='按下按鈕直接聯繫客服',
-                            actions=[
-                                PostbackTemplateAction(
-                                    label='聯繫',
-                                    text='聯繫',
-                                    data='@customer_service'
-                                    )
-                                    ]
-                            )
-                                )
-                                        )
 
-def button_off(event):
-    if isinstance(event, MessageEvent):
-        if event.message.text == "off":
-           
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-            event.reply_token,
-            TemplateSendMessage(
-    alt_text='ButtonsTemplate',
-    template=ButtonsTemplate(
-                            title='Menu',
-                            text='結束客服',
-                            actions=[
-                                PostbackTemplateAction(
-                                    label='聯繫',
-                                    text='聯繫',
-                                    data='@customer_service_off'
-                                    )
-                                    ]
-                            )
-                                )
-                                        )
 
 class write_in_database():
     
     def __init__(self,line_bot_api):
-        
+        dynamodb = boto3.resource('dynamodb')
+        self.table = dynamodb.Table('linebot_try')
         self.__line_bot_api = line_bot_api
         
     #get msg time
@@ -62,17 +21,6 @@ class write_in_database():
         d = datetime.datetime.fromtimestamp(event.timestamp/1000)
         msg_time = d.strftime("%Y-%m-%d %H:%M:%S.%f")
         return msg_time
-    
-    #auto reply
-    def push_msg(self,event):    
-        if isinstance(event, MessageEvent):                 
-                    line_bot_api.push_message("U0a684f7f0a60d1be5dd52c93395c9c89",TextSendMessage(text="客服人員忙線中 "))
-
-    #call dynamodb (in init)
-    def log_in_dynamodb(self):
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('linebot_try')
-        return table
     
     #get user id
     def get_id(self,event):
@@ -89,10 +37,9 @@ class write_in_database():
 
     #judge new custormer or not
     def judge_new_customer(self,user_id: str):
-        table = self.log_in_dynamodb()
-        response = table.get_item(Key={'userid': user_id,})
+        response = self.table.get_item(Key={'userid': user_id,})
         try:
-            response = table.put_item(    
+            response = self.table.put_item(    
                 Item={  "userid"     :  user_id,
                         'funcId'     :  'personal',
                         'group'      :  None,
@@ -133,8 +80,7 @@ class write_in_database():
 
     #record switch   
     def update_item(self,switch,event):
-        table = self.log_in_dynamodb()
-        response = table.update_item(
+        response = self.table.update_item(
             Key={'userid': self.get_id(event)},
             UpdateExpression="set #re  = :r ",
             ExpressionAttributeNames= {'#re' : "record"} ,
@@ -144,12 +90,11 @@ class write_in_database():
     #write msg into table
     def write_in_or_not(self,event):
         user_id = self.get_id(event)
-        table = self.log_in_dynamodb()
-        response = table.get_item(Key={'userid': user_id,})
+        response = self.table.get_item(Key={'userid': user_id,})
         if response["Item"]["record"] == True:
             msgtext_history = response["Item"]["msgtext"]
             msgtext_history.append({self.get_msg_time(event):event.message.text})
-            response = table.put_item(
+            response = self.table.put_item(
                 Item={  "userid"     :  user_id,
                         'funcId'     :  'personal',
                         'group'      :  None,
