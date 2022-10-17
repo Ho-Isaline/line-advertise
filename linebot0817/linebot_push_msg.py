bc817ca13261744c75a61af2a9dfcb211fe47fdb
Ho-Isaline <u980060@gmail.com>
2022-10-17 20:20:12 +0800

    10/17 08:20

--- linebot0817/linebot_push_msg.py
+++
@@ -1,8 +0,0 @@
-from linebot import LineBotApi, WebhookParser
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import *
-
-def push_msg(self,event):
-    if isinstance(event, MessageEvent):
-                print(event)
-                line_bot_api.push_message("U0a684f7f0a60d1be5dd52c93395c9c89",TextSendMessage(text="客服人員忙線中 "))
