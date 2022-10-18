c41777fbd1e5f04d2a0d504a94586fcd72841db1
Ho-Isaline <61371937+Ho-Isaline@users.noreply.github.com>
2022-10-18 14:07:25 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .forms import CreateGroupForm, MyForm
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-from flask import Flask
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import *
 import json
@@ -10,8 +9,6 @@ import boto3
 from django.conf import settings
 from .models import table
 
-
-app = Flask(__name__)
 
 line_bot_api = LineBotApi("1ob3ZSFOu0arzAXk8xh7EnR0x+hOOlJ5p7L603loiIoYP6p0sK1HM+AePqF+pHXEqjTM2PDnXlwN4R3typt1MZHE12JC6n5WLibbMHOA+z7YVp1qQ8sDI2zcn8LcVfEr6APdOhrthUDb13MYGTIyUVGUYhWQfeY8sLGRXgo3xvw=")
 handler = WebhookHandler("3b4bc5fb33633b211c2e19d4f37b9ef5")
